538b87e23af47142184bc7f8c2b42fb25e7fffdb
adityapawar2004 <pawaraditya2901@gmail.com>
2024-11-19 20:23:00 +0530

    added

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ const GET_TICKETS_URL="https://api.quicksell.co/v1/internal/frontend-assignment"
           grouping={grouping}   
           userIdToData={userData}   
         />  </>
-    
+      )}
        
     </div>  
   );  
